df462eb065bd6d06cfac665db3a41d9fe2e8ea15
Adrian <hsg3912@gmail.com>
2026-09-07 13:16:17 +0200

    Solved problem

--- C#/Arrays & Hashing/Contains Duplicate/ContainsDuplicate.Solution/Program.cs
+++ C#/Arrays & Hashing/Contains Duplicate/ContainsDuplicate.Solution/Program.cs
@@ -10,7 +10,19 @@ class Program
 
 public static class Solution {
     public static bool hasDuplicate(int[] nums) {
-       return true;
+        HashSet<int> set = new HashSet<int>();
+
+        if(nums.Length <= 0 || nums.Length > 100000) return false;
+        
+        foreach (int num in nums)
+        {
+            if(set.Contains(num)) {
+                return true;
+            }
+            set.Add(num);
+        }
+        
+        return false;
     }
 }
 
